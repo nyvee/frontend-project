1dc7aaae00ed2067c8991bebab304fdce01bfd72
Randarz <josuaestomihi.7h20@gmail.com>
2023-12-23 00:45:29 +0700

    searchbar updated API connected
    still have some error, ill continue tomorrow

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 
+//import pages
+import 'package:frontend_project/pages/search_bar.dart';
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}): super(key: key);
 
@@ -30,20 +32,27 @@ class HomePageState extends State<HomePage>  {
  }
 
  Widget _buildHeader() {
-   return Container(
-     color: const Color(0xFFF0EBE5), // Set the background color here
-     child: Column(
-       crossAxisAlignment: CrossAxisAlignment.stretch,
-       children: [
-         const SizedBox(height: 70),
-         Row(
-           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-           children: [
-             Row(
-               children: [
-                SizedBox(
-                  width: 250,
-                  height: 40,
+ return Container(
+   color: const Color(0xFFF0EBE5),
+   child: Column(
+     crossAxisAlignment: CrossAxisAlignment.stretch,
+     children: [
+       const SizedBox(height: 70),
+       Row(
+         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+         children: [
+           Row(
+             children: [
+               SizedBox(
+                width: 250,
+                height: 40,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Searchh()),
+                    );
+                  },
                   child: Container(
                     padding: const EdgeInsets.symmetric(horizontal: 5.0),
                     decoration: BoxDecoration(
@@ -54,24 +63,25 @@ class HomePageState extends State<HomePage>  {
                       ),
                       color: const Color(0xFFF0EBE5),
                     ),
-                    child:const Row(
-                    children: [
-                      Icon(Icons.search, color: Color(0xFF31304D)),
-                      SizedBox(width: 5, height: 0,),
-                      Expanded(
-                      child: TextField(
-                        style: TextStyle(fontSize: 18.0, height: 1.0), // Adjust the height value to your preference
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          contentPadding: EdgeInsets.symmetric(vertical: 12.0),
-                          isDense: true,
-                        ),
-                      ),
-                      )
-                    ],
-                    )
+                    child: const Row(
+                      children: [
+                        Icon(Icons.search, color: Color(0xFF31304D)),
+                        SizedBox(width: 5, height: 0,),
+                        Expanded(
+                          child: TextField(
+                            style: TextStyle(fontSize: 18.0, height: 1.0),
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.symmetric(vertical: 12.0),
+                              isDense: true,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
+               ),
                 Padding(
                   padding: const EdgeInsets.only(left: 20, right: 0.0),
                   child: Row(
--- lib/pages/search_bar.dart
+++ lib/pages/search_bar.dart
@@ -1,149 +1,225 @@
-//import dependencies
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_sticky_header/flutter_sticky_header.dart';
+import 'package:http/http.dart' as http;
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_sticky_header/flutter_sticky_header.dart';
+class Product {
+  final String name;
+  final String image;
+  final double price;
+  final String description;
 
-class SearchBar extends StatefulWidget {
-  const SearchBar({Key? key}): super(key: key);
+  Product({required this.name, required this.image, required this.price, required this.description});
+}
+
+class Searchh extends StatefulWidget {
+  const Searchh({Key? key}): super(key: key);
 
   @override
-  SearchBarState createState() => SearchBarState();
+  SearchhState createState() => SearchhState();
 }
 
-class SearchBarState extends State<SearchBar>  {
- @override
-Widget build(BuildContext context) {
- return Scaffold(
-   body: CustomScrollView(
-     slivers: [
-       SliverStickyHeader(
-         header: _buildHeader(),
-       ),
-     ],
-   ),
- );
+class SearchhState extends State<Searchh>  {
+  late List<Product> searchResults = [];
+
+  Future<void> searchProduct(String productName) async {
+  final response = await http.get(Uri.parse('https://gjq3q54r-8080.asse.devtunnels.ms/api/products?name=$productName'));
+
+  if (response.statusCode == 200) {
+    final dynamic responseData = json.decode(response.body);
+
+    if (responseData is List<dynamic>) {
+      final List<Product> filteredResults = responseData
+          .where((item) =>
+              item['name'].toLowerCase().contains(productName.toLowerCase()))
+          .map((item) => Product(
+                name: item['name'],
+                image: item['image'],
+                price: item['price'].toDouble(),
+                description: item['description'],
+              ))
+          .toList();
+
+      setState(() {
+        searchResults = filteredResults;
+      });
+
+      print('Search results loaded: $searchResults');
+    } else if (responseData is Map<String, dynamic>) {
+      // Handle the case when the response is a map
+      // You might need to extract the necessary information from the map
+      // and convert it into a List<Product> as needed.
+    }
+  } else {
+    // Handle error
+    print('Failed to load data');
+  }
 }
 
- Widget _buildHeader() {
-   return Container(
-     color: const Color(0xFFF0EBE5), // Set the background color here
-     child: Column(
-       crossAxisAlignment: CrossAxisAlignment.stretch,
-       children: [
-         const SizedBox(height: 70),
-         Row(
-           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-           children: [
-             Row(
-               children: [
-                SizedBox(
-                  width: 250,
-                  height: 40,
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8.0),
-                      border: Border.all(
-                        color: const Color(0xFF31304D),
-                        width: 2.2,
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverStickyHeader(
+            header: _buildHeader(),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => _buildContent(index),
+              childCount: searchResults.length + 1, // Add 1 for the placeholder
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHeader() {
+    return Container(
+      color: const Color(0xFFF0EBE5),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(height: 70),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                children: [
+                  SizedBox(
+                    width: 250,
+                    height: 40,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        border: Border.all(
+                          color: const Color(0xFF31304D),
+                          width: 2.2,
+                        ),
+                        color: const Color(0xFFF0EBE5),
                       ),
-                      color: const Color(0xFFF0EBE5),
+                      child: Row(
+                        children: [
+                          Icon(Icons.search, color: Color(0xFF31304D)),
+                          SizedBox(width: 5, height: 0,),
+                          Expanded(
+                            child: TextField(
+                              style: TextStyle(fontSize: 18.0, height: 1.0),
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                contentPadding: EdgeInsets.symmetric(vertical: 12.0),
+                                isDense: true,
+                              ),
+                              onSubmitted: (productName) {
+                                if (productName.isNotEmpty) {
+                                  searchProduct(productName);
+                                }
+                              },
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                    child:const Row(
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 0.0),
+                    child: Row(
+                      children: [
+                        _buildIconContainer(Icons.favorite_border),
+                        const SizedBox(width: 18.0),
+                        _buildIconContainer(Icons.notifications_none),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 22),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 390,
+                height: 32,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Handle button press if needed
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF31314D),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      side: const BorderSide(
+                        color:  Color(0xFF31304D),
+                      ),
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Icon(Icons.search, color: Color(0xFF31304D)),
-                      SizedBox(width: 5, height: 0,),
-                      Expanded(
-                      child: TextField(
-                        style: TextStyle(fontSize: 18.0, height: 1.0), // Adjust the height value to your preference
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          contentPadding: EdgeInsets.symmetric(vertical: 12.0),
-                          isDense: true,
+                      const Icon(Icons.discount, size: 17.0),
+                      const SizedBox(width: 5.0),
+                      Text(
+                        'Promotional Offers',
+                        style: GoogleFonts.montserrat(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 12,
                         ),
                       ),
-                      )
-                    ],
-                    )
-
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20, right: 0.0),
-                  child: Row(
-                    children: [
-                      _buildIconContainer(Icons.favorite_border),
-                      const SizedBox(width: 18.0),
-                      _buildIconContainer(Icons.notifications_none),
                     ],
                   ),
                 ),
-               ],
-             ),
-           ],
-         ),
-         const SizedBox(height: 22),
-         Row(
-           mainAxisAlignment: MainAxisAlignment.center,
-           children: [
-             SizedBox(
-               width: 390,
-               height: 32,
-               child: ElevatedButton(
-                onPressed: () {
-                  // Handle button press if needed
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF31314D),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                    side: const BorderSide(
-                      color:  Color(0xFF31304D),
-                    ),
-                  ),
-                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    const Icon(Icons.discount, size: 17.0),
-                    const SizedBox(width: 5.0),
-                    Text(
-                      'Promotional Offers',
-                      style: GoogleFonts.montserrat(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w700,
-                        fontSize: 12,
-                      ),
-                    ),
-                  ],
-                ),
-               ),
-             ),
-           ],
-         ),
-         const SizedBox(height: 20), // Add space below the button
-       ],
-     ),
-   );
- }
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+        ],
+      ),
+    );
+  }
 
+  Widget _buildContent(int index) {
+    if (index < searchResults.length) {
+      final product = searchResults[index];
+      return ListTile(
+        title: Text(product.name),
+        subtitle: Text(product.description),
+        // Other details...
+      );
+    } else if (index == searchResults.length && searchResults.isNotEmpty) {
+    return const SizedBox(
+      child: Text('No more results'),
+    );
+  } else {
+    return const SizedBox(
+      child: Text('Loading...'),
+    );
+  }
+}
 
- Widget _buildIconContainer(IconData iconData) {
-   return Container(
-     width: 40.0,
-     height: 40.0,
-     decoration: BoxDecoration(
-       borderRadius: BorderRadius.circular(8.0),
-       border: Border.all(
-         color: const Color(0xFF31314D),
-         width: 2.2,
-       ),
-       color: Colors.transparent,
-     ),
-     child: Center(
-       child: Icon(iconData, color: const Color(0xFF31314D)),
-     ),
-   );
- }
+  Widget _buildIconContainer(IconData iconData) {
+    return Container(
+      width: 40.0,
+      height: 40.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.0),
+        border: Border.all(
+          color: const Color(0xFF31314D),
+          width: 2.2,
+        ),
+        color: Colors.transparent,
+      ),
+      child: Center(
+        child: Icon(iconData, color: const Color(0xFF31314D)),
+      ),
+    );
+  }
 }
