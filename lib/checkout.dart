be7120d927af408bcb9be230ce62b483f90e3d45
zaenal9671 <153436991+zaenal9671@users.noreply.github.com>
2023-12-26 20:27:26 +0700

    Update checkout.dart

--- lib/checkout.dart
+++ lib/checkout.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CheckoutPage(),
+    );
+  }
+}
+
 class CheckoutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
