3592e83bd12d916b8df91f692fdc268c39429f4e
zaenal9671 <153436991+zaenal9671@users.noreply.github.com>
2023-12-26 19:11:36 +0700

    Create checkout.dart

---
+++ lib/checkout.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class CheckoutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Checkout"),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text("Man's Suit"),
+            subtitle: Text("m | Dark Blue"),
+            trailing: Text("Rp1.990.000"),
+          ),
+          ListTile(
+            title: Text("Man's Suit"),
+            subtitle: Text("m | Dark Blue"),
+            trailing: Text("Rp1.990.000"),
+          ),
+          Divider(),
+          ListTile(
+            title: Text("Delivery and Payment"),
+          ),
+          ListTile(
+            title: Text("Delivery"),
+            subtitle: Text("01233432521234"),
+            trailing: Text("xxxxxxx (your address)"),
+          ),
+          ListTile(
+            title: Text("Payment"),
+            trailing: Text("Bank X"),
+          ),
+          ListTile(
+            title: Text("Total price"),
+            trailing: Text("Rp3.980.000"),
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_cart),
+            title: Text("Buy"),
+            onTap: () {
+              // Do something when the buy button is tapped.
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
