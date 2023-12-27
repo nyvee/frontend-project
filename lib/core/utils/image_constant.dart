e54d1ff6544101f7c54bdd90639ee5bd92d92d68
Ryan dedi Pratama <107033900+pyus72@users.noreply.github.com>
2023-12-27 16:50:38 +0700

    Add files via upload

---
+++ lib/core/utils/image_constant.dart
@@ -0,0 +1,18 @@
+class ImageConstant {
+  // Image folder path
+  static String imagePath = 'assets/images';
+
+  // Wishlist images
+  static String imgArrowLeft = '$imagePath/img_arrow_left.svg';
+
+  static String imgSearch = '$imagePath/img_search.svg';
+
+  // Common images
+  static String imgRectangle27 = '$imagePath/img_rectangle_27.png';
+
+  static String imgFavorite = '$imagePath/img_favorite.svg';
+
+  static String imgCart = '$imagePath/img_cart.svg';
+
+  static String imageNotFound = 'assets/images/image_not_found.png';
+}
